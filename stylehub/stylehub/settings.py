b220028fed6247d37574026b00cba9eb7a037043
13Aniket2003 <duttaaniket637@gmail.com>
2026-02-23 20:02:38 +0530

    all file saved

--- stylehub/stylehub/settings.py
+++ stylehub/stylehub/settings.py
@@ -121,13 +121,10 @@ WSGI_APPLICATION = 'stylehub.wsgi.application'
 #     }
 # }
 import dj_database_url
+import os
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
