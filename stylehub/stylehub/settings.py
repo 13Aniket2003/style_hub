aeda491b444c4d3e0f3d6df1f6823f28a2605e40
13Aniket2003 <duttaaniket637@gmail.com>
2026-02-24 01:07:05 +0530

    all file saved

--- stylehub/stylehub/settings.py
+++ stylehub/stylehub/settings.py
@@ -75,7 +75,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "stylehub" / "static",
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
